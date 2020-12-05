289e5df2ac784c4d18b60175af4cf17c6613b3ab
TraviFravi <travi1500@gmail.com>
2020-12-05 18:03:14 -0500

    added server and Package.json

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.get('/', function(req, res) {
+    res.send("hit route");
+})
+
+app.listen(PORT, function () {
+    console.log("listening for app");
+})
