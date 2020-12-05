e2cec76cf91736e2b17d3ae7996f506483623fa2
TraviFravi <travi1500@gmail.com>
2020-12-05 18:02:43 -0500

    added SQL databse for burgers

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon Burger', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('hawaiian Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('veggie burger', TRUE);
