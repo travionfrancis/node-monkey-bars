a5bba9d7eec6e28fff3a96911c136c34db876d7a
TraviFravi <travi1500@gmail.com>
2020-12-06 03:34:13 -0500

    updating SQL files

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,9 @@
+-- Making sure that the Databse doesn't exist --
 DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
+-- Make table for SQL --
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
@@ -9,3 +11,4 @@ CREATE TABLE burgers
 	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,7 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('bacon Burger', TRUE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('hawaiian Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('veggie burger', TRUE);
+INSERT INTO burgers (burger_name) VALUES ('Pizza Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Complete Burger');
+INSERT INTO burgers (burger_name) VALUES ('Duck Burger');
+-- eaten burgers --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hawaiian Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie burger', TRUE);
